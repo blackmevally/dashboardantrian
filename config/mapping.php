0568cc874bb43e285ad0c046f0f8d91a1cbb5092
blackmevally <blackmevally@gmail.com>
2026-09-11 15:55:26 +0700

    feat: add poli dokter mapping configuration

---
+++ config/mapping.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Kombinasi poli + dokter yang ditampilkan di dashboard.
+ * Isi dengan mapping resmi yang dipakai ruang tunggu.
+ */
+return [
+    // ['kd_poli' => 'U0006', 'kd_dokter' => '8201232007405K'],
+];
